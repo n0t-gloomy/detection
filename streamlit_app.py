b3f38c74a0851f9f218d2503084b7a404c92de53
n0t-gloomy <superscriptionpp@gmail.com>
2026-05-14 15:21:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -100,7 +100,7 @@ if uploaded_file:
 
         if st.button("🚀 Run Classification"):
             with st.spinner("Classifying..."):
-                label, score, annotated = models.classifier(
+                label, score = models.classifier(
                     img,
                     model_data["weights_name"],
                     model_data["class_names"]
